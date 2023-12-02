d4427236a144290469fc23f2d66d569f9221dcab
Harry Roberts <harry.roberts98@gmail.com>
2023-12-02 15:44:19 +0000

    Add access point config screen

--- include/Constants.h
+++ include/Constants.h
@@ -3,12 +3,14 @@
 
 namespace constants
 {
-    const long SecondsOneDay = 86400;
-    const long SecondsOneMonth = 2592000;
-    const long SecondsOneYear = 31536000;
+    inline constexpr long SecondsOneDay = 86400;
+    inline constexpr long SecondsOneMonth = 2592000;
+    inline constexpr long SecondsOneYear = 31536000;
 
-    const float BatteryMaxVoltage = 4.15;
-    const float BatteryMinVoltage = 3.4;
+    inline constexpr float BatteryMaxVoltage = 4.15;
+    inline constexpr float BatteryMinVoltage = 3.4;
+
+    inline constexpr const char* WifiAccessPointName = "Ticker";
 }
 
 #endif
